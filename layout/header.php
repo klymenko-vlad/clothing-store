57ce17f16e9720ac5f8e55f10c64f68c4b6be267
Vlad Klymenko <klymenvlad@gmail.com>
2025-03-24 22:08:13 +0100

    fix: header routings

--- layout/header.php
+++ layout/header.php
@@ -2,25 +2,26 @@
 <header class="">
     <nav class="navbar navbar-expand-lg">
         <div class="container">
-            <a class="navbar-brand" href="index.php"><h2>Sixteen <em>Clothing</em></h2></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand" href="/phpproj/index.php"><h2>Sixteen <em>Clothing</em></h2></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                    aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="index.php">Home
-                            <span class="sr-only">(current)</span>
+                        <a class="nav-link" href="/phpproj/index.php">Home
+<!--                            <span class="sr-only">(current)</span>-->
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="products.php">Our Products</a>
+                        <a class="nav-link" href="/phpproj/products.php">Our Products</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="about.php">About Us</a>
+                        <a class="nav-link" href="/phpproj/about.php">About Us</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="contact.php">Contact Us</a>
+                        <a class="nav-link" href="/phpproj/contact.php">Contact Us</a>
                     </li>
                 </ul>
             </div>
