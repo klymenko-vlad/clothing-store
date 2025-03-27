3ecfc93ce4674a96e736f2cfd26a11c7727be063
Vlad Klymenko <klymenvlad@gmail.com>
2025-03-27 20:29:59 +0100

    feat: dynamic active route changing

--- layout/header.php
+++ layout/header.php
@@ -1,4 +1,8 @@
-<!-- Header -->
+<?php
+$path = $_SERVER['REQUEST_URI'];
+$lastPart = basename(parse_url($path, PHP_URL_PATH));
+?>
+
 <header class="">
     <nav class="navbar navbar-expand-lg">
         <div class="container">
@@ -9,18 +13,17 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
+                    <li class="nav-item <?php echo $lastPart === 'index.php' ? 'active' : ''; ?>">
                         <a class="nav-link" href="/phpproj/index.php">Home
-<!--                            <span class="sr-only">(current)</span>-->
                         </a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item <?php echo $lastPart === 'products.php' ? 'active' : ''; ?>">
                         <a class="nav-link" href="/phpproj/products.php">Our Products</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item <?php echo $lastPart === 'about.php' ? 'active' : ''; ?>">
                         <a class="nav-link" href="/phpproj/about.php">About Us</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item <?php echo $lastPart === 'contact.php' ? 'active' : ''; ?>">
                         <a class="nav-link" href="/phpproj/contact.php">Contact Us</a>
                     </li>
                 </ul>
@@ -28,3 +31,4 @@
         </div>
     </nav>
 </header>
+
